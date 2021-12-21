95ee776cfc5082daf99f710b055d7d20450da33b
Paul DeVito <pdevito3@gmail.com>
2021-12-20 23:08:07 -0500

    fix: resolve merge issue

--- src/HeimGuard/HeimGuardClient.cs
+++ src/HeimGuard/HeimGuardClient.cs
@@ -1,33 +1,38 @@
-namespace HeimGuard;
-
-/// <summary>
-/// The handler that interacts with the policy handler to expose guard methods for your user permissions.
-/// </summary>
-public interface IHeimGuardClient
+namespace HeimGuard
 {
-    /// <summary>
-    /// Determines whether the user has a particular permission.
-    /// </summary>
-    /// <param name="permission">The permission.</param>
-    /// <returns></returns>
-    Task<bool> HasPermissionAsync(string permission);
-}
-
-public class HeimGuardClient : IHeimGuardClient
-{
-    private readonly IUserPolicyHandler _userPolicyHandler;
+    using System.Linq;
+    using System.Threading.Tasks;
 
     /// <summary>
-    /// Initializes a new instance of the <see cref="HeimGuardClient"/> class.
+    /// The handler that interacts with the policy handler to expose guard methods for your user permissions.
     /// </summary>
-    public HeimGuardClient(IUserPolicyHandler userPolicyHandler)
+    public interface IHeimGuardClient
     {
-        _userPolicyHandler = userPolicyHandler;
+        /// <summary>
+        /// Determines whether the user has a particular permission.
+        /// </summary>
+        /// <param name="permission">The permission.</param>
+        /// <returns></returns>
+        Task<bool> HasPermissionAsync(string permission);
     }
 
-    public async Task<bool> HasPermissionAsync(string permission)
+    public class HeimGuardClient : IHeimGuardClient
     {
-        var policy = await _userPolicyHandler.GetUserPermissions();
-        return policy.Contains(permission);
+        private readonly IUserPolicyHandler _userPolicyHandler;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HeimGuardClient"/> class.
+        /// </summary>
+        public HeimGuardClient(IUserPolicyHandler userPolicyHandler)
+        {
+            _userPolicyHandler = userPolicyHandler;
+        }
+
+        public async Task<bool> HasPermissionAsync(string permission)
+        {
+            var policy = await _userPolicyHandler.GetUserPermissions();
+            return policy.Contains(permission);
+        }
     }
 }
+

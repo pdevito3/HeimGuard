e68d3a81fd110655b462ece2519f3b4114b21576
Paul DeVito <pdevito3@gmail.com>
2022-07-30 10:51:34 -0400

    feat: add `MustHavePermission` guard to HeimGuardClient

--- src/HeimGuard/HeimGuardClient.cs
+++ src/HeimGuard/HeimGuardClient.cs
@@ -1,6 +1,9 @@
 namespace HeimGuard
 {
+    using System;
+    using System.Collections.Generic;
     using System.Linq;
+    using System.Security.Claims;
     using System.Threading.Tasks;
 
     /// <summary>
@@ -9,13 +12,26 @@ namespace HeimGuard
     public interface IHeimGuardClient
     {
         /// <summary>
-        /// Determines whether the user has a particular permission.
+        /// Checks if the user has a particular permission.
         /// </summary>
-        /// <param name="permission">The permission.</param>
-        /// <returns></returns>
+        /// <param name="permission"></param>
+        /// <returns>A task that represents the asynchronous permission check. The task result contains
+        /// the true if the user has the given permission and false if they do not.</returns>
         Task<bool> HasPermissionAsync(string permission);
+
+        /// <summary>
+        /// Guards against users without the given permission. Throws an Exception of type
+        /// <see cref="TException"/> if the user does not have the given permission. Does nothing if the
+        /// user has the given permission.
+        /// </summary>
+        /// <param name="permission">The name of the permission the user must have access to.</param>
+        /// <typeparam name="TException">The Exception that should be thrown if a user does not have the given permission.</typeparam>
+        /// <exception cref="TException">User does not have the given permission.</exception>
+        Task MustHavePermission<TException>(string permission)
+            where TException : Exception, new();
     }
 
+    /// <inheritdoc />
     public class HeimGuardClient : IHeimGuardClient
     {
         private readonly IUserPolicyHandler _userPolicyHandler;
@@ -28,10 +44,28 @@ namespace HeimGuard
             _userPolicyHandler = userPolicyHandler;
         }
 
+        /// <inheritdoc />
         public async Task<bool> HasPermissionAsync(string permission)
         {
             var policy = await _userPolicyHandler.GetUserPermissions();
             return policy.Contains(permission);
+        }
+
+        /// <inheritdoc />
+        public async Task MustHavePermission<TException>(string permission) 
+            where TException : Exception, new()
+        {
+            if (!await HasPermissionAsync(permission))
+            {
+                try
+                {
+                    throw Activator.CreateInstance(typeof(TException), permission) as TException;
+                }
+                catch (MissingMethodException)
+                {
+                    throw new TException();
+                }
+            }
         }
     }
 }
